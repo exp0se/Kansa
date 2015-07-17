30e0b52af10b5446f0dbac9517c19f912c311d32
exp0se <takedownz@gmail.com>
2015-07-17 17:39:28 +0300

    Better output

--- Modules/ASEP/Get-SvcAll.ps1
+++ Modules/ASEP/Get-SvcAll.ps1
@@ -4,6 +4,6 @@ Get-SvcAll.ps1 returns information about all Windows services.
 .NOTES
 The following line is required by Kansa.ps1, which uses it to determine
 how to handle the output from this script.
-OUTPUT tsv
+OUTPUT csv
 #>
-Get-WmiObject win32_service | Select-Object Name, DisplayName, PathName, StartName, StartMode, State, TotalSessions, Description 
+Get-WmiObject win32_service | Select-Object Name, DisplayName, Description, PathName, ProcessId, State, StartName
