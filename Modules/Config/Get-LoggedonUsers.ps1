355622f834d400e3697067396eed9b326011a099
exp0se <takedownz@gmail.com>
2015-07-27 11:43:37 +0300

    Get-LoggedonUsers module to show Logged in users

---
+++ Modules/Config/Get-LoggedonUsers.ps1
@@ -0,0 +1,74 @@
+﻿<#
+.Synopsis
+Queries a computer to check for interactive sessions
+
+.DESCRIPTION
+This script takes the output from the quser program and parses this to PowerShell objects
+
+.NOTES   
+Name: Get-LoggedOnUser
+Author: Jaap Brasser
+Version: 1.2
+DateUpdated: 2015-07-07
+
+.LINK
+http://www.jaapbrasser.com
+
+
+.EXAMPLE
+.\Get-LoggedOnUser.ps1 -ComputerName server01,server02
+
+Description:
+Will display the session information on server01 and server02
+
+.EXAMPLE
+'server01','server02' | .\Get-LoggedOnUser.ps1
+
+Description:
+Will display the session information on server01 and server02
+OUTPUT CSV
+#>
+function ConvertTo-Encoding ([string]$From, [string]$To){  
+        Begin{  
+            $encFrom = [System.Text.Encoding]::GetEncoding($from)  
+            $encTo = [System.Text.Encoding]::GetEncoding($to)  
+        }  
+        Process{  
+            $bytes = $encTo.GetBytes($_)  
+            $bytes = [System.Text.Encoding]::Convert($encFrom, $encTo, $bytes)  
+            $encTo.GetString($bytes)  
+        }  
+    } 
+$current_lang = Get-Culture
+        try {
+            if ($current_lang.TwoLetterISOLanguageName -eq "ru") {
+                quser 2>&1 | ConvertTo-Encoding cp866 windows-1251 | Select-Object -Skip 1 | ForEach-Object {
+                    $CurrentLine = $_.Trim() -Replace '\s+',' ' -Split '\s'
+                    $HashProps = @{
+                        UserName = $CurrentLine[0]
+                    }
+
+                    # If session is disconnected different fields will be selected
+                    if ($CurrentLine[2] -eq 'Disc') {
+                            $HashProps.SessionName = $null
+                            $HashProps.Id = $CurrentLine[1]
+                            $HashProps.State = $CurrentLine[2]
+                            $HashProps.IdleTime = $CurrentLine[3]
+                            $HashProps.LogonTime = $CurrentLine[4..6] -join ' '
+                    } else {
+                            $HashProps.SessionName = $CurrentLine[1]
+                            $HashProps.Id = $CurrentLine[2]
+                            $HashProps.State = $CurrentLine[3]
+                            $HashProps.IdleTime = $CurrentLine[4]
+                            $HashProps.LogonTime = $CurrentLine[5..7] -join ' '
+                    }
+
+                    New-Object -TypeName PSCustomObject -Property $HashProps |
+                    Select-Object -Property UserName,SessionName,Id,State,IdleTime,LogonTime,Error
+                }
+            }
+            } catch {
+                New-Object -TypeName PSCustomObject -Property @{
+                    Error = $_.Exception.Message
+                } | Select-Object -Property UserName,SessionName,Id,State,IdleTime,LogonTime,Error
+            }
